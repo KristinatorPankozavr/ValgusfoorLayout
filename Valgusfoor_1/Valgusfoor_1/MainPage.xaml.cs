76e6a7e65723f0cdb0f202fe7d8c2861f39059b9
Kristina Pankova <kristinapankova@outlook.com>
2020-09-13 00:43:03 +0300

    First commit.

---
+++ Valgusfoor_1/Valgusfoor_1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Valgusfoor_1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void offButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
