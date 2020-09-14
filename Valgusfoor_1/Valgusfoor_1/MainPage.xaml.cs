11a82f6cbf78b6ae13ff2642a61cba5ebf77bc64
opilane <opilane@HP-CZC6297B8F>
2020-09-14 15:49:50 +0300

    Valgusfoor

--- Valgusfoor_1/Valgusfoor_1/MainPage.xaml.cs
+++ Valgusfoor_1/Valgusfoor_1/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -10,19 +11,65 @@ namespace Valgusfoor_1
 {
     public partial class MainPage : ContentPage
     {
-
+        bool vkl;
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void onButton_Clicked(object sender, EventArgs e)
+        private async void on_Clicked(object sender, EventArgs e)
         {
-            
+            vkl = true;
+            while (vkl!=false)
+            {
+                punane.BackgroundColor = Color.Red;
+                kollane.BackgroundColor = Color.Gray;
+                roheline.BackgroundColor = Color.Gray;
+                await Task.Run(() => Thread.Sleep(1000));
+                {
+                    kollane.BackgroundColor = Color.Yellow;
+                    punane.BackgroundColor = Color.Gray;
+                    roheline.BackgroundColor = Color.Gray;
+                    await Task.Run(() => Thread.Sleep(1000));
+                }
+                roheline.BackgroundColor = Color.Green;
+                punane.BackgroundColor = Color.Gray;
+                kollane.BackgroundColor = Color.Gray;
+                await Task.Run(() => Thread.Sleep(1000));
+                
+            }
+
+    }
+
+        private void off_Clicked(object sender, EventArgs e)
+        {
+            punane.BackgroundColor = Color.Gray;
+            kollane.BackgroundColor = Color.Gray;
+            roheline.BackgroundColor = Color.Gray;
+            vkl = false;
+        }
+        Random rnd = new Random();
+        private void Random_Clicked(object sender, EventArgs e)
+        {
+            int r = rnd.Next(0, 255);
+            int g = rnd.Next(0, 255);
+            int b = rnd.Next(0, 255);
+            punane.BackgroundColor = Color.FromRgb(r, g, b);
+             r = rnd.Next(0, 255);
+             g = rnd.Next(0, 255);
+             b = rnd.Next(0, 255);
+            kollane.BackgroundColor = Color.FromRgb(r, g, b);
+             r = rnd.Next(0, 255);
+             g = rnd.Next(0, 255);
+             b = rnd.Next(0, 255);
+            roheline.BackgroundColor = Color.FromRgb(r, g, b);
         }
 
-        private void offButton_Clicked(object sender, EventArgs e)
+        private void Oma_Clicked(object sender, EventArgs e)       
         {
+            punane.Opacity = 0.5;
+            kollane.CornerRadius = 0;
+            roheline.CornerRadius = 2;
 
         }
     }
